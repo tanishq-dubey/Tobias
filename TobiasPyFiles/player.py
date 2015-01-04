60043afa2d2cabd0f47457939481c74f88b1dd57
Tanishq Dubey <tanishq.dubey@gmail.com>
2015-01-03 18:48:14 -0600

    Added a attack module and supporting defs

--- TobiasPyFiles/player.py
+++ TobiasPyFiles/player.py
@@ -1,20 +1,32 @@
 
-from TobiasPyFiles import items
+from TobiasPyFiles import items, enemies
 
 __author__ = 'Tanishq Dubey'
 
 class Player:
 
-    inventory = []
+    itemInventory = []
+    weaponInventory = [items.Weapons]
 
     hp = 100
+
+    chosenWeapon = None
+
+    ap = 0
 
     def isPlayerAlive(self):
         return self.hp > 0
 
-    def printInventory(self):
-        for item in self.inventory:
+    def isPlayerUsingArmor(self):
+        return self.ap > 0
+
+    def printItemInventory(self):
+        for item in self.itemInventory:
             print(item, '\n')
+
+    def printWeaponInventory(self):
+        for item in self.weaponInventory:
+            print(items.Weapons, '\n')
 
     def move(self, dx, dy):
         self.locationX += dx
@@ -31,3 +43,33 @@ class Player:
 
     def moveWest(self):
         self.move(dx=-1, dy=0)
+
+    def PlayerSetWeapon(self,weaponNumber):
+        self.chosenWeapon = weaponNumber
+
+    def CheckPlayerWeapon(self):
+        if(self.chosenWeapon is None):
+            print("You don't have a weapon equipped! Now would be a decent time to choose one!")
+            return False
+        else:
+            return True
+
+    def PlayerAttack(self,enemy):
+        if self.CheckPlayerWeapon():
+            damage = self.weaponInventory[self.chosenWeapon].damage
+            if enemy.useArmor():
+                enemy.ap -= damage
+                if enemy.ap <= 0:
+                    print("You use your{} against {}, and destroy their armor!")
+                else:
+                    print("You use your {} against {}, dealing {} damage to their armor!".format(self.weaponInventory[self.chosenWeapon], enemy.name, damage))
+            else:
+                enemy.hp -= damage
+                if enemy.isAlive():
+                    print("You use your {} against {}, dealing {} damage to them!".format(self.weaponInventory[self.chosenWeapon], enemy.name, damage))
+                else:
+                    print("You use your {} against {}, dealing enough damage to kill them!".format(self.weaponInventory[self.chosenWeapon], enemy.name))
+
+        else:
+            self.CheckPlayerWeapon()
+            return
